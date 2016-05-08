08118fb0690cd27bc678e26f19369019126693fb
CALEB STAUFFER <develop+gitkraken@calebstauffer.com>
2016-05-08 00:56:48 -0400

    add security

--- query-monitor-extend.php
+++ query-monitor-extend.php
@@ -8,6 +8,12 @@ Author: Caleb Stauffer
 Author URI: http://develop.calebstauffer.com
 */
 
+if (!defined('ABSPATH') || !function_exists('add_filter')) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
 new css_qm_extend;
 class css_qm_extend {
 
