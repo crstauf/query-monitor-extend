a5782eed7e6d1ef5733294951c27cc7ba8e1896c
Caleb Stauffer <develop@calebstauffer.com>
2016-07-12 23:52:22 -0400

    add security

--- output-html.php
+++ output-html.php
@@ -1,5 +1,11 @@
 <?php
 
+if (!defined('ABSPATH') || !function_exists('add_filter')) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
 class CSS_QM_Output_Html_Constants extends QM_Output_Html {
 
 	public function __construct( QM_Collector $collector ) {
