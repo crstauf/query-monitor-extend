642b7d9960c42752a1acd403a491745c6d5a637f
Caleb Stauffer <develop@calebstauffer.com>
2016-07-29 23:26:11 -0400

    cleanup Included Files collector and output

--- output/included_files.php
+++ output/included_files.php
@@ -6,7 +6,7 @@
 
 class CSSLLC_QMX_Output_Html_IncludedFiles extends QM_Output_Html {
 
-    public static $errors = 0;
+    var $data = array();
 
     public function __construct( QM_Collector $collector ) {
         parent::__construct( $collector );
@@ -14,17 +14,32 @@ class CSSLLC_QMX_Output_Html_IncludedFiles extends QM_Output_Html {
         add_filter( 'qm/output/title', array( $this, 'admin_title' ), 101 );
     }
 
-    public function output() {
-        $data = $this->collector->get_data();
+    public function hide_core_files() {
+        return defined( 'QMX_HIDE_CORE_FILES' ) && QMX_HIDE_CORE_FILES;
+    }
 
-        if (
-            !array_key_exists( 'files', $data )
-            || !is_array( $data['files'] )
-            || !count( $data['files'] )
-        )
-            return;
+    public function collect_data() {
+        $this->data['components'] = array();
+
+        foreach ( get_included_files() as $file ) {
+            $component = QM_Util::get_file_component( $file )->name;
+            if ( $this->hide_core_files() && 'Core' === $component )
+                continue;
+
+            if (array_key_exists($component,$this->data['components']))
+                $this->data['components'][$component]++;
+            else
+                $this->data['components'][$component] = 1;
+
+            $this->data['files'][$file] = array(
+                'component' => $component,
+                'filesize' => filesize( $file ),
+                'selectors' => $this->get_selectors( $file ),
+            );
+        }
 
         $php_errors = QM_Collectors::get( 'php_errors' )->get_data();
+
         if (
             array_key_exists( 'errors', $php_errors )
             && is_array( $php_errors['errors'] )
@@ -32,7 +47,12 @@ class CSSLLC_QMX_Output_Html_IncludedFiles extends QM_Output_Html {
             && is_array( $php_errors['errors']['warning'] )
             && count( $php_errors['errors']['warning'] )
         )
-            foreach ( $php_errors['errors']['warning'] as $error )
+            foreach ( $php_errors['errors']['warning'] as $error ) {
+
+                $component = QM_Util::get_file_component( $error->file )->name;
+                if ( $this->hide_core_files() && 'Core' === $component )
+                    continue;
+
                 if (false !== stripos( $error->message, 'No such file or directory' ) ) {
                     foreach ( array(
                         'include(',
@@ -51,33 +71,94 @@ class CSSLLC_QMX_Output_Html_IncludedFiles extends QM_Output_Html {
                     if ( false === $length )
                         continue;
 
-                    self::$errors++;
-
                     $include_file = substr( $error->message, $start, $length );
-                    $component = QM_Util::get_file_component( $error->file )->name;
 
                     $selectors = array();
                     foreach ( array(
                         'including' => $error->file,
-                        'included' => $include_file,
+                        'included'  => $include_file,
                     ) as $status => $file )
-                        $selectors = array_merge( CSSLLC_QMX_Collector_IncludedFiles::get_selectors( $file, 'including' === $status ), $selectors );
+                        $selectors = array_merge(
+                            $this->get_selectors( $file, 'including' === $status ),
+                            $selectors
+                        );
 
-                    $data['errors'][$include_file] = array(
-                        'filesize' => filesize( $error->file ),
-                        'component' => $component,
-                        'including' => $error->file,
+                    $this->data['errors'][$include_file] = array(
+                        'filesize'       => filesize( $error->file ),
+                        'component'      => $component,
+                        'including'      => $error->file,
                         'including_line' => $error->line,
-                        'selectors' => $selectors,
+                        'selectors'      => $selectors,
                     );
                 }
+            }
+    }
+
+    public function get_selectors( $file, $above_or_root = true ) {
+        $path = dirname( str_replace( ABSPATH, '', $file ) );
+        $selectors = array();
+
+        if ( $above_or_root && strlen( trailingslashit( dirname( $file ) ) ) < strlen( ABSPATH ) ) {
+            $selectors['__above-install'] = 1;
+            $path = $file;
+        } else {
+            if ( $above_or_root && ABSPATH === trailingslashit( dirname( $file ) ) )
+                $selectors['__root'] = 1;
+            $path = str_replace( DIRECTORY_SEPARATOR, '-', $path );
+            $path = str_replace( '_', '-', $path );
+            $path = explode( '-', $path );
+
+            if ( is_array( $path ) && count( $path ) )
+                foreach ( $path as $piece ) {
+                    if ( in_array( $piece, array( '', '.', ' ' ) ) )
+                        continue;
+                    $selectors[$piece] = 1;
+                }
+        }
+
+        return $selectors;
+    }
+
+    /**
+    * Adapted from:
+    * http://stackoverflow.com/a/2510459
+    */
+    private function format_bytes_to_kb( $bytes, $precision = 2 ) {
+        $bytes = max( $bytes, 0 );
+        $bytes /= pow( 1000, 1 );
+
+        return round( $bytes, $precision );
+    }
+
+    public function output() {
+        $this->collect_data();
+
+        if (
+            (
+                !array_key_exists( 'files', $this->data )
+                || !is_array( $this->data['files'] )
+                || !count( $this->data['files'] )
+            ) && (
+                !array_key_exists( 'errors', $this->data )
+                || !is_array( $this->data['errors'] )
+                || !count( $this->data['errors'] )
+            )
+        )
+            return;
 
         $selectors = array();
-        foreach ( $data['files'] as $details )
-            $selectors = array_merge( $details['selectors'], $selectors );
-        if ( array_key_exists( 'errors', $data ) && is_array( $data['errors'] ) && count( $data['errors'] ) )
-            foreach ( $data['errors'] as $details )
-                $selectors = array_merge( $details['selectors'], $selectors );
+
+        foreach ( array( 'files', 'errors' ) as $status )
+            if (
+                array_key_exists( $status, $this->data )
+                && is_array( $this->data[$status] )
+                && count( $this->data[$status] )
+                && array_key_exists( 'selectors', $this->data[$status] )
+                && is_array( $this->data[$status]['selectors'] )
+                && count( $this->data[$status]['selectors'] )
+            )
+                foreach ( $this->data[$status] as $details )
+                    $selectors = array_merge( $details['selectors'], $selectors );
 
         echo '<div id="' . esc_attr( $this->collector->id() ) . '" class="qm">';
 
@@ -85,7 +166,17 @@ class CSSLLC_QMX_Output_Html_IncludedFiles extends QM_Output_Html {
 				'<thead>' .
 					'<tr>' .
 						'<th colspan="4">Included Files' .
-                            '<label class="qmx-filter-hide" style="float: right;"><input type="checkbox" data-filter="includedfilescomponent" value="Core" /> Hide Core</label>' .
+                            '<label class="qmx-filter-hide' .
+                                (
+                                    $this->hide_core_files()
+                                    ? ' qm-info" title="QMX_HIDE_CORE_FILES constant is true'
+                                    : ''
+                                ) . '" style="float: right;"><input type="checkbox" data-filter="includedfilescomponent" value="Core"' .
+                                (
+                                    $this->hide_core_files()
+                                    ? ' checked="checked" disabled="disabled"'
+                                    : ''
+                                ) . ' /> Hide core files</label>' .
                         '</th>' .
 					'</tr>' .
 					'<tr>' .
@@ -106,41 +197,68 @@ class CSSLLC_QMX_Output_Html_IncludedFiles extends QM_Output_Html {
                         '</th>' .
 						'<th class="qm-num qmx-includedfiles-filesize">Filesize' . $this->build_sorter() . '</th>' .
 						'<th class="qmx-includedfiles-component">Component' .
-                            $this->build_filter( 'includedfilescomponent', array_keys( $data['components'] ) ) .
+                            $this->build_filter( 'includedfilescomponent', array_keys( $this->data['components'] ) ) .
 						'</th>' .
 					'</tr>' .
 				'</thead>' .
                 '<tbody>';
 
-                    if ( array_key_exists( 'errors', $data ) && is_array( $data['errors'] ) && count( $data['errors'] ) )
-                        foreach ($data['errors'] as $path => $details) {
-                            echo '<tr data-qm-includedfilespath="' . esc_attr( implode( ' ', array_keys( $details['selectors'] ) ) ) . '" data-qm-includedfilescomponent="' . esc_attr( $details['component'] ) . '" class="qm-warn">' .
-                                '<td class="qm-num" data-qm-sort-value="0"> </td>' .
-                                '<td data-qm-sort-value="' . esc_attr( str_replace( '.php', '', strtolower( $path ) ) ) . '">' .
-                                    esc_html( $path ) .  '<br />' .
-                                    '<span class="qm-info">&nbsp;' . esc_html( $details['including'] . ':' . $details['including_line'] ) .
-                                '</td>' .
-                                '<td class="qm-num qmx-includedfiles-filesize"> </td>' .
-                                '<td class="qmx-includedfiles-component">' . esc_html( $details['component'] ) . '</td>' .
-                            '</tr>';
-                        }
-
                     $count = $filesize = 0;
 
-					foreach ($data['files'] as $path => $details) {
-                        $count++;
-                        $filesize = $filesize + $details['filesize'];
-                        echo '<tr data-qm-includedfilespath="' . esc_attr( implode( ' ', array_keys( $details['selectors'] ) ) ) . '" data-qm-includedfilescomponent="' . esc_attr( $details['component'] ) . '">' .
-                            '<td class="qm-num">' . esc_html( $count ) . '</td>' .
-                            '<td data-qm-sort-value="' . esc_attr( str_replace( '.php', '', strtolower( $path ) ) ) . '">' .
-                                ( strlen( trailingslashit( dirname( $path ) ) ) < strlen( ABSPATH )
-                                ? esc_html( $path )
-                                : '<abbr title="' . esc_attr( $path ) . '">./' . esc_html( str_replace( ABSPATH, '', $path) ) . '</abbr>') .
-                            '</td>' .
-                            '<td class="qm-num qmx-includedfiles-filesize" data-qm-sort-value="' . esc_attr( $details['filesize'] ) . '">' . esc_html( number_format_i18n( $details['filesize'] / 1024, 2 ) ) . ' KB</td>' .
-                            '<td>' . esc_html( $details['component'] ) . '</td>' .
-                        '</tr>';
-					}
+                    foreach ( array( 'errors', 'files' ) as $status )
+                        if (
+                            array_key_exists( $status, $this->data )
+                            && is_array( $this->data[$status] )
+                            && count( $this->data[$status] )
+                        )
+                            foreach ( $this->data[$status] as $path => $details ) {
+                                if ( 'files' === $status ) {
+                                    $count++;
+                                    $filesize = $filesize + intval( $details['filesize'] );
+                                }
+                                echo '<tr ' .
+                                        'data-qm-includedfilespath="' . esc_attr( implode( ' ', array_keys( $details['selectors'] ) ) ) . '" ' .
+                                        'data-qm-includedfilescomponent="' . esc_attr( $details['component'] ) . '"' .
+                                        ( 'errors' === $status ? ' class="qm-warn"' : '' ) .
+                                    '>' .
+                                        '<td class="qm-num" data-qm-sort-value="' . ( 'errors' === $status ? 0 : $count ) . '">' .
+                                            ( 'errors' === $status ? ' ' : $count ) .
+                                        '</td>' .
+                                        '<td data-qm-sort-value="' . esc_attr( str_replace( '.php', '', strtolower( $path ) ) ) . '">' .
+                                            (
+                                                'errors' === $status
+                                                    || strlen( trailingslashit( dirname( $path ) ) ) < strlen( ABSPATH )
+                                                ? esc_html( $path ) .
+                                                    (
+                                                        array_key_exists( 'including', $details)
+                                                        && array_key_exists( 'including_line', $details )
+                                                        ? '<br /><span class="qm-info">&nbsp;' . esc_html( $details['including'] . ':' . $details['including_line'] )
+                                                        : ''
+                                                    )
+                                                : '<abbr title="' . esc_attr( $path ) . '">' .
+                                                    './' . esc_html( str_replace( ABSPATH, '', $path) ) .
+                                                '</abbr>'
+                                            ) .
+                                        '</td>' .
+                                        '<td ' .
+                                            'class="qm-num qmx-includedfiles-filesize" ' .
+                                            'data-qm-sort-value="' .
+                                                (
+                                                    'errors' === $status
+                                                    ? '0'
+                                                    : esc_attr( $details['filesize'] )
+                                                ) .
+                                            '"' .
+                                        '>' .
+                                            (
+                                                'errors' === $status
+                                                ? ' '
+                                                : esc_html( number_format_i18n( $details['filesize'] / 1024, 2 ) ) . ' KB'
+                                            ) .
+                                        '</td>' .
+                                        '<td class="qmx-includedfiles-component">' . esc_html( $details['component'] ) . '</td>' .
+                                '</tr>';
+                            }
 
 				echo '</tbody>' .
 				'<tfoot>' .
@@ -159,31 +277,45 @@ class CSSLLC_QMX_Output_Html_IncludedFiles extends QM_Output_Html {
     }
 
     public function admin_title( array $title ) {
-        $data = $this->collector->get_data();
 
         $title[] = sprintf(
             _x( '%s%s<small>F</small>', 'number of included files', 'query-monitor' ),
-            array_key_exists( 'files', $data ) && is_array( $data['files'] ) ? count( $data['files'] ) : 0,
-            0 !== self::$errors ? '/' . self::$errors : ''
+            (
+                array_key_exists( 'files', $this->data ) && is_array( $this->data['files'] )
+                ? count( $this->data['files'] )
+                : 0
+            ),
+            (
+                0 !== count( $this->data['errors'] )
+                ? '/' . count( $this->data['errors'] )
+                : ''
+            )
         );
 
         return $title;
     }
 
     public function admin_menu( array $menu ) {
-        $data = $this->collector->get_data();
 
         $add = array(
             'id'    => 'qm-included_files',
             'href'  => '#qm-included_files',
             'title' => sprintf(
                 __( 'Included files (%s%s)', 'query-monitor' ),
-                is_array( $data['files'] ) ? count( $data['files'] ) : 0,
-                0 !== self::$errors ? ' / ' . self::$errors : ''
+                (
+                    is_array( $this->data['files'] )
+                    ? count( $this->data['files'] )
+                    : 0
+                ),
+                (
+                    0 !== count( $this->data['errors'] )
+                    ? ' / ' . count( $this->data['errors'] )
+                    : ''
+                )
             )
         );
 
-        if ( 0 !== self::$errors )
+        if ( 0 !== count( $this->data['errors'] ) )
             $add['meta']['classname'] = 'qm-alert';
 
         $menu[] = $this->menu( $add );
