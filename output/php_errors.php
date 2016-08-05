0d3f60d5306e5e128716cc46e5b20a8fb35cbde1
Caleb Stauffer <develop@calebstauffer.com>
2016-08-04 22:18:31 -0400

    add security

--- output/php_errors.php
+++ output/php_errors.php
@@ -1,5 +1,11 @@
 <?php
 
+if (!defined('ABSPATH') || !function_exists('add_filter')) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
 /* based on QM v2.12.0 */
 class QMX_Output_Html_PHP_Errors extends QM_Output_Html_PHP_Errors {
 
