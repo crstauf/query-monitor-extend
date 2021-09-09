c048b34efd422f31ad46fad070e8bf15ed9419f2
Caleb Stauffer <develop+github@calebstauffer.com>
2021-09-09 14:13:49 -0400

    breakup collectors and outputters

---
+++ Constants/qmx-collector--constants.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Query Monitor Constants collector.
+ */
+
+add_action( 'plugin_loaded', 'load_qmx_constants_collector' );
+
+function load_qmx_constants_collector( string $file ) {
+
+	if ( 'query-monitor/query-monitor.php' !== plugin_basename( $file ) )
+		return;
+
+	remove_action( 'plugin_loaded', __FUNCTION__ );
+
+	if ( !class_exists( 'QueryMonitor' ) )
+		return;
+
+	class QMX_Collector_Constants extends QM_Collector {
+
+		public $id = 'constants';
+
+		public function name() {
+			return __( 'Constants', 'query-monitor-extend' );
+		}
+
+		public function process() {
+
+			$constants = get_defined_constants( true );
+			$this->data['constants'] = $constants['user'];
+
+		}
+
+	}
+
+	add_filter( 'qm/collectors', static function ( array $collectors ) : array {
+		$collectors['constants'] = new QMX_Collector_Constants;
+		return $collectors;
+	} );
+
+}
---
+++ Time/qmx-collector--time.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Query Monitor Time collector.
+ */
+
+add_action( 'plugin_loaded', 'load_qmx_time_collector' );
+
+function load_qmx_time_collector( string $file ) {
+
+	if ( 'query-monitor/query-monitor.php' !== plugin_basename( $file ) )
+		return;
+
+	remove_action( 'plugin_loaded', __FUNCTION__ );
+
+	if ( !class_exists( 'QueryMonitor' ) )
+		return;
+
+	class QMX_Collector_Time extends QM_Collector {
+
+		public $id = 'time';
+
+		public function name() {
+			return __( 'Time', 'query-monitor-extend' );
+		}
+
+		function process() {
+
+			$this->data['functions'] = array(
+				'UTC'       => 'get_utc',
+				'Server'    => 'get_server',
+				'WordPress' => 'get_wp',
+				'Browser'   => 'get_browser',
+			);
+
+		}
+
+		function get_utc() {
+			$datetime = date_create( "now", new DateTimeZone( 'UTC' ) );
+			$datetime->setTimezone( new DateTimeZone( 'UTC' ) );
+			return $datetime->format( 'D, M j, Y H:i:s' );
+		}
+
+		function get_server() {
+			$datetime = date_create( "now", new DateTimeZone( 'UTC' ) );
+
+			if ( !empty( ini_get( 'date.timezone' ) ) )
+				$datetime->setTimezone( new DateTimeZone( ini_get( 'date.timezone' ) ) );
+
+			return $datetime->format( 'D, M j, Y H:i:s T' );
+		}
+
+		function get_server_offset() {
+			$datetime = date_create( "now", new DateTimeZone( 'UTC' ) );
+
+			if ( !empty( ini_get( 'date.timezone' ) ) )
+				$datetime->setTimezone( new DateTimeZone( ini_get( 'date.timezone' ) ) );
+
+			return $datetime->format( 'Z' );
+		}
+
+		function get_server_timezone() {
+			$datetime = date_create( "now", new DateTimeZone( 'UTC' ) );
+
+			if ( !empty( ini_get( 'date.timezone' ) ) )
+				$datetime->setTimezone( new DateTimeZone( ini_get( 'date.timezone' ) ) );
+
+			return $datetime->format( 'T' );
+		}
+
+		function get_wp() {
+			return current_time( 'D, M j, Y H:i:s T' );
+		}
+
+		function get_wp_offset() {
+			return get_option( 'gmt_offset' );
+		}
+
+		function get_wp_timezone() {
+			return current_time( 'T' );
+		}
+
+		function get_browser() {
+			return '-';
+		}
+
+	}
+
+	add_filter( 'qm/collectors', static function ( array $collectors ) : array {
+		$collectors['time'] = new QMX_Collector_Time;
+		return $collectors;
+	} );
+
+}
