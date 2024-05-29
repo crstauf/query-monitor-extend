2b77ef83a7aa7ccd6b075be9bd039eefa2586a44
Caleb Stauffer <develop+github@calebstauffer.com>
2024-05-29 00:26:00 -0400

    save

--- qmx-time-hooks.php
+++ qmx-time-hooks.php
@@ -13,11 +13,15 @@ function qmx_time_hooks( string $start_lap_stop ) : void {
 
 	global $wp_filter;
 
-	$label    = 'Action: ' . current_action();
+	$label    = current_action();
 	$action   = sprintf( 'qm/%s', $start_lap_stop );
-	$priority = $wp_filter[ current_action() ]->current_priority();
+	$priority = '';
 
-	do_action( $action, $label );
+	if ( 'lap' === $start_lap_stop ) {
+		$priority = $wp_filter[ current_action() ]->current_priority();
+	}
+
+	do_action( $action, $label, $priority );
 }
 
 /**
@@ -42,5 +46,6 @@ function qmx_stop() : void {
 }
 
 add_action( 'init', 'qmx_start', 0 );
-add_action( 'init', 'qmx_lap', 5 );
-add_action( 'init', 'qmx_stop', 50 );
+add_action( 'init', 'qmx_lap', 9 );
+add_action( 'init', 'qmx_lap', 11 );
+add_action( 'init', 'qmx_stop', 25 );
